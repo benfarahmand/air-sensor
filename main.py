712a8001b308f0dd8b9940ab15cd7461def9aa3d
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-04 18:22:29 -0800

    Update main.py

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ class AirSensor:
 
 	def __init__(self):
 		self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+		self.mq2 = [] # LPG, Propane and Hydrogen
+		self.mq3 = [] # Alcohol, Ethanol
+		self.mq4 = [] # Methane, Natural Gas
+		self.mq5 = [] # LPG, Natural Gas
+		self.mq6 = [] # LPG, Iso-butane, Propane
+		self.mq7 = [] # Carbon Monoxide
+		self.mq8 = [] # Hydrogen
+		self.mq9 = [] # Methan, Propane, and CO
+		self.mq135 = [] # NH3, NOx, Alcohol, Benzene, Smoke, CO2
 
 	def getDataFromArduino(self):
 		# to do
@@ -13,9 +22,19 @@ class AirSensor:
 
 	def parseData(self, raw):
 		# to do
-		# result = re.search(":(.*)-",raw)
 		result = raw[raw.find(": ")+1 : raw.find(" -")]
 		return result
+
+	def storeDataInArray(self, data):
+		self.mq2 = [] # LPG, Propane and Hydrogen
+		self.mq3 = [] # Alcohol, Ethanol
+		self.mq4 = [] # Methane, Natural Gas
+		self.mq5 = [] # LPG, Natural Gas
+		self.mq6 = [] # LPG, Iso-butane, Propane
+		self.mq7 = [] # Carbon Monoxide
+		self.mq8 = [] # Hydrogen
+		self.mq9 = [] # Methan, Propane, and CO
+		self.mq135 = [] # NH3, NOx, Alcohol, Benzene, Smoke, CO2
 
 	# def saveData(data):
 		# to do
@@ -32,7 +51,8 @@ class AirSensor:
 				# print(raw)
 				data = self.parseData(raw)
 				if data is not None:
-					print(data)
+					print(data[0])
+					# storeDataInArray(data)
 			# saveData(data)
 			# visualizeData(data)
 
