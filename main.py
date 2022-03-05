e8afd4ec25f2e819355fd3a85beb82adac4d68b0
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-04 17:55:08 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 import serial
 import re
 
-def getDataFromArduino(ser):
-	# to do
-	if ser.in_waiting > 0:
-		return ser.readline().decode('utf-8').rstrip()
+class AirSensor:
+	
+	def getDataFromArduino(ser):
+		# to do
+		if ser.in_waiting > 0:
+			return ser.readline().decode('utf-8').rstrip()
 
-def parseData(raw):
-	# to do
-	result = re.search(":(.*)-",raw)
-	return result
+	def parseData(raw):
+		# to do
+		result = re.search(":(.*)-",raw)
+		return result
 
-def saveData(data):
-	# to do
+	# def saveData(data):
+		# to do
 
-def visualizeData(data):
-	# to do
+	# def visualizeData(data):
+		# to do
 
-def main():
-	ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-	ser.reset_input_buffer()
-	while True:
-		raw = getDataFromArduino(ser)
-		data = parseData(raw)
-		print(data)
-		# saveData(data)
-		# visualizeData(data)
+	def main():
+		ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+		ser.reset_input_buffer()
+		while True:
+			raw = getDataFromArduino(ser)
+			data = parseData(raw)
+			print(data)
+			# saveData(data)
+			# visualizeData(data)
 
 a = main()
 a()
