6d2753e7f98ffdc3c311ba5af3d67dacc3f8be0b
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-04 17:54:00 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
 import serial
-if __name__ == '__main__':
-    ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
-    ser.reset_input_buffer()
-    while True:
-        if ser.in_waiting > 0:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line.len())
+import re
+
+def getDataFromArduino(ser):
+	# to do
+	if ser.in_waiting > 0:
+		return ser.readline().decode('utf-8').rstrip()
+
+def parseData(raw):
+	# to do
+	result = re.search(":(.*)-",raw)
+	return result
+
+def saveData(data):
+	# to do
+
+def visualizeData(data):
+	# to do
+
+def main():
+	ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
+	ser.reset_input_buffer()
+	while True:
+		raw = getDataFromArduino(ser)
+		data = parseData(raw)
+		print(data)
+		# saveData(data)
+		# visualizeData(data)
+
+a = main()
+a()
