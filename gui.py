c6172e9614471dd5e101f7e9b90c2579c0708c9e
benfarahmand <ben@tuvalabs.com>
2022-03-05 22:04:27 -0500

    working on gui
    working on gui

---
+++ gui.py
@@ -0,0 +1,37 @@
+import pygame as pg
+
+def gui:
+
+	def __init__(self):
+		pg.init()
+		self.myfont = pg.font.SysFont("monospace", 15)
+
+		
+		#define some colors
+		self.BLACK = (0,0,0)
+		self.WHITE = (255,255,255)
+		self.BLUE = (0, 0, 255)
+		self.GREEN = (0, 255, 0)
+		self.RED = (255, 0, 0)
+
+		self.PI = 3.141592653
+
+		self.screenWidth = 800
+		self.screenHeight = 480
+
+		self.size = (self.screenWidth, self.screenHeight)
+		self.screen = pg.display.set_mode(self.size, pg.FULLSCREEN)
+
+		# Used to manage how fast the screen updates
+		self.clock = pg.time.Clock()
+
+	def draw(self, time, mq2):
+		self.screen.fill(self.WHITE)
+		i = 0 
+		# while i < len(time)
+		# 	pg.draw.line(screen, self.BLACK,(time[i],mq2[i]['GAS_LPG']),(screenWidth*0.9,screenHeight*0.9))
+
+		pg.display.flip()
+
+# def clickEvent():
+
--- main.py
+++ main.py
@@ -9,21 +9,22 @@ from MQ7PPM import MQ7PPM
 from MQ8PPM import MQ8PPM
 from MQ9PPM import MQ9PPM
 from MQ135PPM import MQ135PPM
+from gui import gui
 
 class AirSensor:
 
 	def __init__(self):
 		self.ser = serial.Serial('/dev/ttyACM0', 115200, timeout=1)
 		self.timeStamp = []
-		self.mq2 = [] # LPG, Propane and Hydrogen
-		self.mq3 = [] # Alcohol, Ethanol
-		self.mq4 = [] # Methane, Natural Gas
-		self.mq5 = [] # LPG, Natural Gas
-		self.mq6 = [] # LPG, Iso-butane, Propane
-		self.mq7 = [] # Carbon Monoxide
-		self.mq8 = [] # Hydrogen
-		self.mq9 = [] # Methan, Propane, and CO
-		self.mq135 = [] # NH3, NOx, Alcohol, Benzene, Smoke, CO2
+		self.mq2 = [] 
+		self.mq3 = [] 
+		self.mq4 = [] 
+		self.mq5 = [] 
+		self.mq6 = [] 
+		self.mq7 = [] 
+		self.mq8 = [] 
+		self.mq9 = [] 
+		self.mq135 = [] 
 		self.mq2ppm = MQ2PPM()
 		self.mq3ppm = MQ3PPM()
 		self.mq4ppm = MQ4PPM()
@@ -33,6 +34,7 @@ class AirSensor:
 		self.mq8ppm = MQ8PPM()
 		self.mq9ppm = MQ9PPM()
 		self.mq135ppm = MQ135PPM()
+		self.gui = gui()
 
 	def getDataFromArduino(self):
 		# to do
@@ -49,45 +51,37 @@ class AirSensor:
 			index = int(data[0 : data.find(":")])
 			value = int(data[data.find(":")+2 : len(data)])
 			if index == 2:
-				# self.timeStamp.append(datetime.datetime.now().time())
+				self.timeStamp.append(datetime.datetime.now().time())
 				print("TIME: "+str(datetime.datetime.now().time()))
 				print("MQ2:"+str(self.mq2ppm.getMQPPM(value)))
-				# self.mq2.append(value)
-			if index == 3:
-				print("MQ3:"+str(self.mq3ppm.getMQPPM(value)))
-				# self.mq3.append(value)
-			if index == 4:
-				print("MQ4:"+str(self.mq4ppm.getMQPPM(value)))
-				# self.mq4.append(value)
-			if index == 5:
-				print("MQ5:"+str(self.mq5ppm.getMQPPM(value)))
-				# self.mq5.append(value)
-			if index == 6:
-				print("MQ6:"+str(self.mq6ppm.getMQPPM(value)))
-				# self.mq6.append(value)
-			if index == 7:
-				print("MQ7:"+str(self.mq7ppm.getMQPPM(value)))
-				# self.mq7.append(value)
-			if index == 8:
-				print("MQ8:"+str(self.mq8ppm.getMQPPM(value)))
-				# self.mq8.append(value)
-			if index == 9:
-				print("MQ9:"+str(self.mq9ppm.getMQPPM(value)))
-				# self.mq9.append(value)
-			if index == 135:
-				print("MQ135:"+str(self.mq135ppm.getMQPPM(value)))
+				self.mq2.append(self.mq2ppm.getMQPPM(value))
+				print(self.mq2[len(self.mq2)-1]['GAS_LPG'])
+			# if index == 3:
+			# 	print("MQ3:"+str(self.mq3ppm.getMQPPM(value)))
+			# 	# self.mq3.append(value)
+			# if index == 4:
+			# 	print("MQ4:"+str(self.mq4ppm.getMQPPM(value)))
+			# 	# self.mq4.append(value)
+			# if index == 5:
+			# 	print("MQ5:"+str(self.mq5ppm.getMQPPM(value)))
+			# 	# self.mq5.append(value)
+			# if index == 6:
+			# 	print("MQ6:"+str(self.mq6ppm.getMQPPM(value)))
+			# 	# self.mq6.append(value)
+			# if index == 7:
+			# 	print("MQ7:"+str(self.mq7ppm.getMQPPM(value)))
+			# 	# self.mq7.append(value)
+			# if index == 8:
+			# 	print("MQ8:"+str(self.mq8ppm.getMQPPM(value)))
+			# 	# self.mq8.append(value)
+			# if index == 9:
+			# 	print("MQ9:"+str(self.mq9ppm.getMQPPM(value)))
+			# 	# self.mq9.append(value)
+			# if index == 135:
+			# 	print("MQ135:"+str(self.mq135ppm.getMQPPM(value)))
 				# self.mq135.append(value)
 		except:
 			print("An exception occurred")
-		
-	# def convertVoltageToPPM(self, data):
-		# to do
-
-	# def saveData(data):
-		# to do
-
-	# def visualizeData(data):
-		# to do
 
 	def __call__(self):
 		
@@ -102,8 +96,8 @@ class AirSensor:
 						data = self.parseData(raw)
 						if data is not None and len(data)>0 and len(data)<10:
 							self.storeDataInArray(data)
-					# saveData(data)
-					# visualizeData(data)
+							# if len(self.timeStamp) > 3:
+							# 	self.gui.draw(self.timeStamp, self.mq2)
 		except:
 			print("Exiting")
 
