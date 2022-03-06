1955645b7a2e42d0dbacfdfc92fd8b0e8892bceb
benfarahmand <ben@tuvalabs.com>
2022-03-05 22:34:21 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -3,7 +3,7 @@ import pygame as pg
 class gui:
 
 	def __init__(self):
-		self.pg.init()
+		pg.init()
 		self.myfont = pg.font.SysFont("monospace", 15)
 
 		
@@ -20,24 +20,24 @@ class gui:
 		self.screenHeight = 480
 
 		self.size = (self.screenWidth, self.screenHeight)
-		self.screen = self.pg.display.set_mode(self.size, self.pg.FULLSCREEN)
+		self.screen = pg.display.set_mode(self.size, pg.FULLSCREEN)
 
 		# Used to manage how fast the screen updates
-		self.clock = self.pg.time.Clock()
+		self.clock = pg.time.Clock()
 
 	def draw(self, time, mq2):
 		self.screen.fill(self.WHITE)
 		i = 0 
 		while i < len(time) - 1:
-			self.pg.draw.line(self.screen, self.RED , (i,round(mq2[i]['GAS_LPG'])) , (i + 1,round(mq2[i + 1]['GAS_LPG'])))
-			self.pg.draw.line(self.screen, self.GREEN , (i,round(mq2[i]['CARBON_MONOXIDE'])) , (i + 1,round(mq2[i + 1]['CARBON_MONOXIDE'])))
-			self.pg.draw.line(self.screen, self.BLUE , (i,round(mq2[i]['SMOKE'])) , (i + 1,round(mq2[i + 1]['SMOKE'])))
+			pg.draw.line(self.screen, self.RED , (i,round(mq2[i]['GAS_LPG'])) , (i + 1,round(mq2[i + 1]['GAS_LPG'])))
+			pg.draw.line(self.screen, self.GREEN , (i,round(mq2[i]['CARBON_MONOXIDE'])) , (i + 1,round(mq2[i + 1]['CARBON_MONOXIDE'])))
+			pg.draw.line(self.screen, self.BLUE , (i,round(mq2[i]['SMOKE'])) , (i + 1,round(mq2[i + 1]['SMOKE'])))
 			i += 1
 
-		self.pg.display.flip()
+		pg.display.flip()
 
-		for event in self.pg.event.get():
-			if event.type == self.pg.MOUSEBUTTONDOWN:
+		for event in pg.event.get():
+			if event.type == pg.MOUSEBUTTONDOWN:
 				print("Mouse Event Detected")
 				return True
 
