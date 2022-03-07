0a11a747140388a07a841f60c3f9e342640e847c
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-06 19:05:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -105,38 +105,38 @@ class AirSensor:
 		quit = False
 		self.seconds = time.time() #when we started the program
 
-		# try:
-		while quit == False:
-			if self.ser.isOpen():
-				raw = self.getDataFromArduino()
-				if raw is not None:
-					# print(raw)
-					data = self.parseData(raw)
-					if data is not None and len(data)>0 and len(data)<10:
-						self.storeDataInArray(data)
-						l = 4
-						if (len(self.timeStamp) > l and 
-							len(self.mq2) > l and len(self.mq3) > l and len(self.mq4) > l and
-							len(self.mq5) > l and len(self.mq6) > l and len(self.mq7) > l and
-							len(self.mq8) > l and len(self.mq9) > l and len(self.mq135) > l):
-							quit = self.gui.draw(
-								self.timeStamp, 
-								[self.mq2,self.mq3,self.mq4,
-								self.mq5,self.mq6,self.mq7,
-								self.mq8,self.mq9,self.mq135],
-								self.maxGraphTime,
-								[self.mq2ppm.MAX_PPM,self.mq3ppm.MAX_PPM,self.mq4ppm.MAX_PPM,
-								self.mq5ppm.MAX_PPM,self.mq6ppm.MAX_PPM,self.mq7ppm.MAX_PPM,
-								self.mq8ppm.MAX_PPM,self.mq9ppm.MAX_PPM,self.mq135ppm.MAX_PPM],
-								[self.mq2ppm.MIN_PPM,self.mq3ppm.MIN_PPM,self.mq4ppm.MIN_PPM,
-								self.mq5ppm.MIN_PPM,self.mq6ppm.MIN_PPM,self.mq7ppm.MIN_PPM,
-								self.mq8ppm.MIN_PPM,self.mq9ppm.MIN_PPM,self.mq135ppm.MIN_PPM],
-								[self.mq2ppm.LABEL,self.mq3ppm.LABEL,self.mq4ppm.LABEL,
-								self.mq5ppm.LABEL,self.mq6ppm.LABEL,self.mq7ppm.LABEL,
-								self.mq8ppm.LABEL,self.mq9ppm.LABEL,self.mq135ppm.LABEL])
-							self.secondsPassed = (self.timeStamp[len(self.timeStamp)-1] - self.timeStamp[0])
-		# except:
-		# 	print("Exiting")
+		try:
+			while quit == False:
+				if self.ser.isOpen():
+					raw = self.getDataFromArduino()
+					if raw is not None:
+						# print(raw)
+						data = self.parseData(raw)
+						if data is not None and len(data)>0 and len(data)<10:
+							self.storeDataInArray(data)
+							l = 4
+							if (len(self.timeStamp) > l and 
+								len(self.mq2) > l and len(self.mq3) > l and len(self.mq4) > l and
+								len(self.mq5) > l and len(self.mq6) > l and len(self.mq7) > l and
+								len(self.mq8) > l and len(self.mq9) > l and len(self.mq135) > l):
+								quit = self.gui.draw(
+									self.timeStamp, 
+									[self.mq2,self.mq3,self.mq4,
+									self.mq5,self.mq6,self.mq7,
+									self.mq8,self.mq9,self.mq135],
+									self.maxGraphTime,
+									[self.mq2ppm.MAX_PPM,self.mq3ppm.MAX_PPM,self.mq4ppm.MAX_PPM,
+									self.mq5ppm.MAX_PPM,self.mq6ppm.MAX_PPM,self.mq7ppm.MAX_PPM,
+									self.mq8ppm.MAX_PPM,self.mq9ppm.MAX_PPM,self.mq135ppm.MAX_PPM],
+									[self.mq2ppm.MIN_PPM,self.mq3ppm.MIN_PPM,self.mq4ppm.MIN_PPM,
+									self.mq5ppm.MIN_PPM,self.mq6ppm.MIN_PPM,self.mq7ppm.MIN_PPM,
+									self.mq8ppm.MIN_PPM,self.mq9ppm.MIN_PPM,self.mq135ppm.MIN_PPM],
+									[self.mq2ppm.LABEL,self.mq3ppm.LABEL,self.mq4ppm.LABEL,
+									self.mq5ppm.LABEL,self.mq6ppm.LABEL,self.mq7ppm.LABEL,
+									self.mq8ppm.LABEL,self.mq9ppm.LABEL,self.mq135ppm.LABEL])
+								self.secondsPassed = (self.timeStamp[len(self.timeStamp)-1] - self.timeStamp[0])
+		except:
+			print("Exiting")
 
 a = AirSensor()
 a()
