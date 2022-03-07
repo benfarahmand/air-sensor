90e2b5d00e89ccb5354ff5344a7b69e92fc2e53f
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-06 19:38:56 -0800

    Update MQ3PPM.py

--- MQ3PPM.py
+++ MQ3PPM.py
@@ -2,15 +2,20 @@ import time
 import math
 
 class MQ3PPM():
-
-    RL_VALUE                     = 4.7#200      # define the load resistance on the board, in kilo ohms
-    RO_CLEAN_AIR_FACTOR          = 1#60     # RO_CLEAR_AIR_FACTOR=(Sensor resistance in clean air)/RO,
-                                            # which is derived from the chart in datasheet
-    # MIN_PPM = 0.05 #unclear if this is accurate. got it from data sheet.
-    # MAX_PPM = 10
-    MIN_PPM = 50 #unclear if this is accurate. got it from data sheet.
-    MAX_PPM = 500
     LABEL = "MQ3"
+    
+    # v1                                    
+    RL_VALUE                     = 200      
+    RO_CLEAN_AIR_FACTOR          = 60
+    MIN_PPM = 0.05 #unclear if this is accurate. got it from data sheet.
+    MAX_PPM = 10
+
+    #v2
+    # RL_VALUE                     = 4.7
+    # RO_CLEAN_AIR_FACTOR          = 1
+    # MIN_PPM = 50 #unclear if this is accurate. got it from data sheet.
+    # MAX_PPM = 500
+    
 
     def __init__(self):
         self.Ro = self.RO_CLEAN_AIR_FACTOR
@@ -18,8 +23,8 @@ class MQ3PPM():
         # following values are derived from the logarithmic graphs 
         # from the datasheets format: [x, y, slope]
         # then in another equation below we will use these values to determine the ppm
-        # self.AlcoholCurve = [-1.0,0.36,-0.64] #v1
-        self.AlcoholCurve = [1.7,-0.74,-0.91] #v2
+        self.AlcoholCurve = [-1.0,0.36,-0.64] #v1
+        # self.AlcoholCurve = [1.7,-0.74,-0.91] #v2
     
     def getMQPPM(self, raw):
         val = {}
