c732bb56ce692e828737289c1f9886e8ffcb69a0
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-07 19:07:55 -0800

    super init

--- MQFunctions.py
+++ MQFunctions.py
@@ -5,7 +5,7 @@ class MQFunctions(object):
 
     CALIBRATION_SAMPLE_TIMES = 50
 
-    def __init__():
+    def __init__(self):
         self.Ro = self.RO_CLEAN_AIR_FACTOR
         self.calibrationValue = 0.0
         self.calibrationSampleCount = 0
