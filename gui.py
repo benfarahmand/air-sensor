6a8d9226288be4dec52fb9d2b24c56a3bc7da298
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-09 18:03:47 -0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -64,11 +64,31 @@ class gui:
 			sLabel = self.myfont.render("Calibrating... "+str(calibrationSampleCount),1,self.BLACK)
 			self.screen.blit(sLabel,(x+5,y))
 		else:
-			i = 0
-			while (i < len(data) - 1 and i < len(time) - 1 and len(data)>4):
+			# i = 0
+			# while (i < len(data) - 1 and i < len(time) - 1 and len(data)>4):
+			# 	# scale the lines to the appropirate width and height
+			# 	x1 = self.translate(time[i],time[0],maxX+time[0],0,-width)+x
+			# 	x2 = self.translate(time[i+1],time[0],maxX+time[0],0,-width)+x
+			# 	# print("i: "+str(i))
+			# 	# if there are multiple lines per sensor, draw all the lines
+			# 	j = 0 
+			# 	for gas in data[i]:
+			# 		# print(sensorLabel +": "+str(gas))
+			# 		# print("checking data length "+str(len(data[i][gas])))
+			# 		y1 = self.translate(round(data[i][gas]),0,maxY,0,height-self.fontsize)
+			# 		y2 = self.translate(round(data[i + 1][gas]),0,maxY,0,height-self.fontsize)
+			# 		pg.draw.line(self.screen, self.graphColors[j], (x1,y+height-y1-self.fontsize),(x2,y+height-y2-self.fontsize))
+			# 		if i == 0:
+			# 			ppmLabel = self.myfont.render(str(gas)+":"+str(round(data[i][gas]))+"ppm",1,self.graphColors[j])
+			# 			# self.screen.blit(ppmLabel,(x2+5+j*ppmLabel.get_width(),y+height/2-self.fontsize/2))
+			# 			self.screen.blit(ppmLabel,(x+j*ppmLabel.get_width(),y-self.fontsize/2))
+			# 		j+=1
+			# 	i += 1
+			i = len(data)-1
+			while (i > 0 and len(data)>4):
 				# scale the lines to the appropirate width and height
 				x1 = self.translate(time[i],time[0],maxX+time[0],0,-width)+x
-				x2 = self.translate(time[i+1],time[0],maxX+time[0],0,-width)+x
+				x2 = self.translate(time[i-1],time[0],maxX+time[0],0,-width)+x
 				# print("i: "+str(i))
 				# if there are multiple lines per sensor, draw all the lines
 				j = 0 
@@ -76,14 +96,14 @@ class gui:
 					# print(sensorLabel +": "+str(gas))
 					# print("checking data length "+str(len(data[i][gas])))
 					y1 = self.translate(round(data[i][gas]),0,maxY,0,height-self.fontsize)
-					y2 = self.translate(round(data[i + 1][gas]),0,maxY,0,height-self.fontsize)
+					y2 = self.translate(round(data[i - 1][gas]),0,maxY,0,height-self.fontsize)
 					pg.draw.line(self.screen, self.graphColors[j], (x1,y+height-y1-self.fontsize),(x2,y+height-y2-self.fontsize))
 					if i == 0:
 						ppmLabel = self.myfont.render(str(gas)+":"+str(round(data[i][gas]))+"ppm",1,self.graphColors[j])
 						# self.screen.blit(ppmLabel,(x2+5+j*ppmLabel.get_width(),y+height/2-self.fontsize/2))
 						self.screen.blit(ppmLabel,(x+j*ppmLabel.get_width(),y-self.fontsize/2))
 					j+=1
-				i += 1
+				i -= 1
 
 
 	def draw(self, time, sensorArray, maxX):
