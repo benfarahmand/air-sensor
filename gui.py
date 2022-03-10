d611f94092b69c53af159d30180528e2521ba63e
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-09 19:16:53 -0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -62,8 +62,8 @@ class gui:
 			i = 0
 			while (i < len(data) - 1 and i < len(time) - 1 and len(data)>4):
 				# scale the lines to the appropirate width and height
-				x1 = self.translate(time[i],time[0],maxX+time[0],0,width)+x
-				x2 = self.translate(time[i+1],time[0],maxX+time[0],0,width)+x
+				x1 = self.screenWidth - self.translate(time[i],time[0],maxX+time[0],0,width)
+				x2 = self.screenWidth - self.translate(time[i+1],time[0],maxX+time[0],0,width)
 				# print("i: "+str(i))
 				# if there are multiple lines per sensor, draw all the lines
 				if(i==0):
