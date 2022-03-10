531c3770c1b32800c7dc1ee787f7f85f512ae80e
Benjamin Farahmand <ipreferyahoo@gmail.com>
2022-03-09 18:34:26 -0800

    Update gui.py

--- gui.py
+++ gui.py
@@ -67,10 +67,12 @@ class gui:
 			i = 0
 			timeL = len(time)
 			dataL = len(data)
+			adjustX = 0
 			while (i < dataL - 1 and i < timeL - 1 and dataL>4):
 				# scale the lines to the appropirate width and height
-				x1 = self.translate(time[i],time[0],maxX+time[0],width,0)
-				x2 = self.translate(time[i+1],time[0],maxX+time[0],width,0)
+				x1 = self.translate(time[i],time[0],maxX+time[0],width,0) - adjustX
+				x2 = self.translate(time[i+1],time[0],maxX+time[0],width,0) - adjustX
+				adjustX = x1
 				# print("i: "+str(i))
 				# if there are multiple lines per sensor, draw all the lines
 				j = 0 
@@ -79,11 +81,11 @@ class gui:
 					# print("checking data length "+str(len(data[i][gas])))
 					y1 = self.translate(round(data[i][gas]),0,maxY,0,height-self.fontsize)
 					y2 = self.translate(round(data[i + 1][gas]),0,maxY,0,height-self.fontsize)
-					pg.draw.line(self.screen, self.graphColors[j], (x2,y+height-y2-self.fontsize),(x1,y+height-y1-self.fontsize))
+					pg.draw.line(self.screen, self.graphColors[j], (x2-adjustX[],y+height-y2-self.fontsize),(x1-adjustX[],y+height-y1-self.fontsize))
 					if i == dataL - 2:
 						ppmLabel = self.myfont.render(str(gas)+":"+str(round(data[i][gas]))+"ppm",1,self.graphColors[j])
 						# self.screen.blit(ppmLabel,(x2+5+j*ppmLabel.get_width(),y+height/2-self.fontsize/2))
-						self.screen.blit(ppmLabel,(x+j*ppmLabel.get_width()/2,y-self.fontsize/2))
+						self.screen.blit(ppmLabel,(j*ppmLabel.get_width(),y+height/2))
 					j+=1
 				i += 1
 
